6f1c007f10aa7cabe0c4c484d4129b3d4942f0e6
Duy Nguyen <thavananh@gmail.com>
2025-08-13 20:06:51 +0700

    fix: resolve path issues by adding configuration file

--- template/head.php
+++ template/head.php
@@ -1,6 +1,5 @@
 <?php
-// Đặt đúng tên folder của bạn trên webserver, ví dụ '/COURSEPRO1'
-define('BASE_URL', '/CoursePro1');
+require_once __DIR__ . '/../config.php'; // lên 1 cấp để tới project root
 
 ?>
 <!DOCTYPE html>
@@ -11,9 +10,9 @@ define('BASE_URL', '/CoursePro1');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Course Online</title>
     <!-- CSS từ public/css -->
-    <link href="<?= BASE_URL ?>/public/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= BASE_URL ?>/public/css/style.css" rel="stylesheet">
-    <link href="<?= BASE_URL ?>/public/css/font_awesome_all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo BASE_URI; ?>public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URI; ?>public/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URI; ?>public/css/font_awesome_all.min.css">
 </head>
 
 <body>
